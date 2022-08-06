f864e86ba52283cdda019c0f082a6349f3fe5c33
P Luong <p-luong@live.co.uk>
2022-08-06 15:58:24 +0100

    created function to get location coordinates

--- assets/search.js
+++ assets/search.js
@@ -3,29 +3,56 @@ var submitButton = document.querySelector("#search-form");
 // get weather api url using fetch() .then() methods
 
 /* var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=8d2766b941018d7a3ac5440bf33f1fc2';
-var url = 'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=8d2766b941018d7a3ac5440bf33f1fc2';
+var url = 'http://api.openweathermap.org/geo/1.0/direct?q=London&appid=8d2766b941018d7a3ac5440bf33f1fc2';
 console.log(requestUrl);
 console.log(url);
  */
-function getApi() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=8d2766b941018d7a3ac5440bf33f1fc2';
+
+function getApi(lat, lon) {
+// TODO: Loop through the data and generate your HTML
+    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=hourly,daily&appid=8d2766b941018d7a3ac5440bf33f1fc2';
     console.log(requestUrl);
+
     fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+        
+    })
+
+    for (var i = 0; i < data.length; i++) {
+        var login = document.createElement('h3');
+        var url = document.createElement('p');
+        login.textContent = data[i].login;
+        url.textContent = data[i].url;
+        userContainer.append(login);
+        userContainer.append(url);
+    }
+}
+
+function getCoordinates(search) {
+    var coordinates = 'http://api.openweathermap.org/geo/1.0/direct?q=' + search + '&appid=8d2766b941018d7a3ac5440bf33f1fc2';
+    console.log(coordinates);
+
+    fetch(coordinates)
       .then(function (response) {
         return response.json();
       })
-      .then(function (data) {
-        // Use the console to examine the response
-        console.log(data);
-        // TODO: Loop through the data and generate your HTML
-/*         for (var i = 0; i < data.length; i++) {
-          var login = document.createElement('h3');
-          var url = document.createElement('p');
-          login.textContent = data[i].login;
-          url.textContent = data[i].url;
-          userContainer.append(login);
-          userContainer.append(url);
-        } */
+      .then(function (location) {
+        console.log(location);
+
+        if (!location.length) {
+            console.log("No results found!");
+        }
+
+        var lat = location[0].lat;
+        var lon = location[0].lon;
+        console.log(lat);
+        console.log(lon);
+        
+        getApi(lat, lon);
       });
   }
 
@@ -39,7 +66,7 @@ function searchFormSubmit() {
         return;
     }
 
-    getApi(searchInput);
+    getCoordinates(searchInput);
 }
 
 submitButton.addEventListener('submit', searchFormSubmit);
